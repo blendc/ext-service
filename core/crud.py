750cc7eae4665fd9910a2f1eb3ac2038902ce4b2
Blendi <144025216+blendc@users.noreply.github.com>
2025-02-21 14:03:26 +0100

    pre-curds

--- core/crud.py
+++ core/crud.py
@@ -5,3 +5,9 @@ def get_object_or_404(model_class, **kwargs):
     if not obj:
         request_not_found(error="{} Not found".format(model_class.__name__))
     return obj
+
+def get_object_or_400(model_class, **kwargs):
+    obj = model_class.get_or_none(**kwargs)
+    if not obj:
+        request_bad(error="{} Not found".format(model_class.__name__))
+    return obj
